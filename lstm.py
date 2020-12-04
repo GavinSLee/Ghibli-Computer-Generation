2c033be7231231410b08bdc0e3f61231731beaa5
Andrew Kim <andrewdkim@users.noreply.github.com>
2020-12-03 23:37:39 -0500

    reduced to 40 epochs

--- lstm.py
+++ lstm.py
@@ -120,7 +120,7 @@ def train(model, network_input, network_output):
     )
     callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, epochs=100, batch_size=128, callbacks=callbacks_list)
+    model.fit(network_input, network_output, epochs=40, batch_size=128, callbacks=callbacks_list)
 
 if __name__ == "__main__":    
     #load files in
