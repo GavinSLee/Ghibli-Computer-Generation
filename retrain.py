b832f97070288b256dc6e1ae1c3e8e761d7bb337
Andrew Kim <andrewdkim@users.noreply.github.com>
2020-12-05 17:55:27 -0500

    retrain

--- retrain.py
+++ retrain.py
@@ -17,8 +17,6 @@ if __name__ == "__main__":
 
     model = create_network(network_input, n_vocab)
 
-    model.train_on_batch(network_input, network_output)
-
     model.fit(network_input, network_output, epochs=1, batch_size=128, shuffle=True)
 
     model.load_weights("weights-060-4.7109.hdf5")
